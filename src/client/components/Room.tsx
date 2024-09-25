9ed30af6fe46390a340b73d0db4e640526c7c8a5
OscarGlo <gloaguen.oscar@gmail.com>
2024-09-25 13:29:37 +0200

    ✨ User list toggle

--- src/client/components/Room.tsx
+++ src/client/components/Room.tsx
@@ -3,18 +3,36 @@ import { WebSocketProvider } from "../contexts/WebSocket";
 import React, { useContext } from "react";
 import { useParams } from "react-router";
 import { PasswordContext } from "../contexts/Password";
-import { Box, Stack } from "@mui/material";
+import { Box, IconButton, Stack } from "@mui/material";
 import { GameProvider } from "../contexts/Game";
 import { UserList } from "./UserList";
+import { useToggle } from "../hooks/useToggle";
+import People from "@mui/icons-material/People";
 
 export function Room() {
   const { id } = useParams();
   const { password } = useContext(PasswordContext);
+
+  const [userListOpen, toggleUserListOpen] = useToggle(true);
+
   return (
     <WebSocketProvider query={{ id, password }}>
       <GameProvider>
         <Stack direction="row" sx={{ width: "100%", height: "100%" }}>
-          <UserList />
+          {userListOpen ? <UserList /> : null}
+
+          <Box padding={1}>
+            <IconButton onClick={toggleUserListOpen} sx={{ minHeight: 0 }}>
+              <People
+                color={
+                  (userListOpen ? "primary" : "disabled") as
+                    | "primary"
+                    | "disabled"
+                }
+              />
+            </IconButton>
+          </Box>
+
           <Box flex={1} overflow="auto" textAlign="center" paddingTop={6}>
             <GameBoard />
           </Box>
