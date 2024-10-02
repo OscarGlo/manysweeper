b3f27c95ed42248a091fda61f17ddfd16b7b0469
OscarGlo <gloaguen.oscar@gmail.com>
2024-10-02 14:26:29 +0200

    🐛 Cookie rerender

--- src/client/contexts/Cookies.tsx
+++ src/client/contexts/Cookies.tsx
@@ -1,10 +1,13 @@
 import cookie from "cookie";
-import React, { createContext, useCallback, useState } from "react";
+import React, { createContext, useCallback, useEffect, useState } from "react";
 import { WithChildren } from "../util/WithChildren";
 
 export interface CookiesContextValue {
   cookies: Record<string, string>;
-  setCookie: (key: string, value: string) => void;
+  setCookie: (
+    key: string,
+    value: string | ((prev: string | undefined) => string),
+  ) => void;
 }
 
 export const CookiesContext = createContext<CookiesContextValue>({
@@ -17,16 +20,25 @@ export function CookiesProvider({
 }: WithChildren): React.ReactElement {
   const [cookies, setCookies] = useState(cookie.parse(document.cookie ?? ""));
 
-  const setCookie = useCallback(
-    (key: string, value: string) => {
-      document.cookie = cookie.serialize(key, value, {
-        maxAge: 30 * 24 * 60 * 60,
-        path: "/",
+  useEffect(() => {
+    console.log(cookies);
+  }, [cookies]);
+
+  const setCookie: CookiesContextValue["setCookie"] = useCallback(
+    (key, value) => {
+      setCookies((cookies) => {
+        const actualValue =
+          typeof value === "string" ? value : value(cookies[key]);
+        console.log(actualValue);
+        document.cookie = cookie.serialize(key, actualValue, {
+          maxAge: 30 * 24 * 60 * 60,
+          path: "/",
+        });
+        cookies[key] = actualValue;
+        return { ...cookies };
       });
-      cookies[key] = value;
-      setCookies({ ...cookies });
     },
-    [cookies, setCookies],
+    [setCookies],
   );
 
   return (
